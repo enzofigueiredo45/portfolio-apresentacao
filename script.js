f235638d8783b99c46a752be124cd01d0141f07d
Enzo Figueiredo <102247630+enzofigueiredo45@users.noreply.github.com>
2024-03-30 20:48:49 -0300

    primeiro laçamento
    dia 30.03.2024

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Enzo Figueiredo", "Paulista", "Desenvolvedor FrontEnd", "Técnico em Mecatrônica"],
+    typeSpeed: 100, 
+    backSpeed: 100, 
+    backDelay: 1000,
+    loop: true
+})
